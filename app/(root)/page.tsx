fc5c5f0c8790c1c87fd3ffde54bd9cbb7ebffb4b
Kamil Hayat <kamilhayat27@gmail.com>
2025-04-15 16:27:31 +0530

    `Refactor codebase: update dependencies, remove unused files, and modify various functions and components`

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,38 @@
-import Image from 'next/image';
-import { UserButton } from '@clerk/nextjs';
-export default function Home() {
+import React from 'react';
+import { fetchAllThread } from '@/lib/actions/thread.action';
+import { currentUser } from '@clerk/nextjs/server';
+import ThreadCard from '@/components/cards/ThreadCard';
+const page = async () => {
+  const { posts, isNext } = await fetchAllThread(1, 20)
+  const user = await currentUser();
+  // console.log(posts);
   return (
-    <div>
-      <h1 className='text-white'>
-        home
-      </h1>
-    </div>
+    <>
+      <h1 className='text-white'>home</h1>
+      <section className='flex flex-col gap-4'>
+        {posts.length===0 ?(
+          <p>No threads found</p>
+        ):(
+          <>
+            {posts.map((post)=>(
+              <ThreadCard 
+                key={post._id}
+                id={post._id}
+                currentUser={user?.id || ''}
+                content={post.text}
+                author={post.author}
+                createdAt={post.createdAt}
+                // community={post.community}
+                // parentId={post.parentId}
+                comments={post.children}
+              />
+            ))}
+          </>
+        )}
+
+      </section>
+    </>
   );
-}
-  
+};
+
+export default page;
